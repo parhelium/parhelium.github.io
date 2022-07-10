4a85ed6a93c5d14febcfa9cc29942f3a82564841
kw <kuba@wyrobek.net>
2022-07-10 23:45:31 +0200

    2022-07-10-23:45 | 6a40682 | fixed links | kw

---
+++ _app/chunks/stores-10c25052.js
@@ -0,0 +1,1 @@
+import{w as t}from"./index-a42be050.js";const o=window.localStorage.getItem("prefersLight")==="true",r=t(o);r.subscribe(e=>{window.localStorage.setItem("prefersLight",e)});export{r as l};
